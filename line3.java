f6f41232d9eac099b1fe93bab283b76433d60b0c
shradh90 <shradhad1996@gmail.com>
2021-06-15 17:51:16 +0530

    using oops cconcept and compareTo method compare lengths

---
+++ line3.java
@@ -0,0 +1,52 @@
+package line;
+/* using compare to method check lines are equal or greater than or less than of two lines based on end points , so that i know when 2 lines are eual
+ * @author: Shradha Desai
+ * @since: 14/06/2021
+ */
+import java.util.Scanner;
+public class lineComparision {
+	 public static void calculateLength() {
+		 // give inputs to the co-ordinates
+		 	Scanner scn = new Scanner(System.in);
+		 	System.out.println("enter point  x1 ");
+	        int x1 = scn.nextInt();
+	        System.out.println("enter a point y1 ");
+	        int y1 = scn.nextInt();
+	        System.out.println("enter point  x2 ");
+	        int x2 = scn.nextInt();
+	        System.out.println("enter a point y2 ");
+	        int y2 = scn.nextInt();
+	        
+	        System.out.println("enter point  a1 ");
+	        int a1 = scn.nextInt();
+	        System.out.println("enter a point b1 ");
+	        int b1 = scn.nextInt();
+	        System.out.println("enter point  a2 ");
+	        int a2 = scn.nextInt();
+	        System.out.println("enter a point b2 ");
+	        int b2 = scn.nextInt();
+	       // calculate length 1 and length 2 
+	        double l1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+	        System.out.println("length 1 :  " + l1);
+	        double l2 = Math.sqrt((a2 - a1) * (a2 - a1) + (b2 - b1) * (b2 - b1));
+	        System.out.println("length 2 :  " + l2);
+	        //compare lines using compareTo method
+	       if(l1 == l2) {
+	    	   System.out.println("line l1 is equal to l2 ");
+	       } else {
+	    	   System.out.println("line l1 is not equal to l2 "); 
+	       }
+	        String line1 = String.valueOf(l1);
+	        String line2 = String.valueOf(l2);
+	        if(line1.compareTo(line2) == 0) {
+	        	 System.out.println("line l1 is equal to l2 ");
+	        } else if(line1.compareTo(line2) < 0) {
+	        	 System.out.println("length of line l1 is less than l2 ");
+	        } else  if(line1.compareTo(line2) > 0) {
+	        	 System.out.println("line l1 is greater than l2 ");
+	        }
+	 }
+	        public static void main(String[] args) {
+	        calculateLength();      	
+  }
+}
